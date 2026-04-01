06c1e9881c7c8bd299f34164622a92ef01081f6b
VladTrokun <trokunvlad@gmail.com>
2026-04-01 20:53:17 +0300

    Laba 4

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Plant, ServiceRequest
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    # Відображаємо назву, ціну, наявність та дати
+    list_display = ('name', 'category', 'price', 'is_available', 'created_at', 'updated_at')
+    # Додаємо фільтрацію справа
+    list_filter = ('is_available', 'category', 'created_at')
+    # Можливість пошуку за назвою
+    search_fields = ('name',)
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'email', 'created_at')
+    readonly_fields = ('created_at',) # Робимо дату тільки для читання
--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('page1/', views.plants),
-    path('page2/', views.about),
+    path('plants/', views.plants),
+    path('about/', views.about),
 ]
