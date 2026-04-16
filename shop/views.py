d4439add07bb91a03286a66565e0a86482fae379
VladTrokun <trokunvlad@gmail.com>
2026-04-16 22:20:48 +0300

    Laba 5

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
+from .models import Plant, Category
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+
+    context = {
+        'title': 'Vita Garden',
+        'description': 'Магазин кімнатних рослин',
+        'categories': categories
+    }
+
+    return render(request, 'home.html', context)
 
 
 def plants(request):
-    return render(request, 'page1.html')
+    category_id = request.GET.get('category')
+
+    if category_id:
+        plants = Plant.objects.filter(category_id=category_id)
+    else:
+        plants = Plant.objects.all()
+
+    categories = Category.objects.all()
+
+    return render(request, 'page1.html', {
+        'plants': plants,
+        'categories': categories
+    })
 
 
 def about(request):
-    return render(request, 'page2.html')
+    categories = Category.objects.all()
 
-
+    return render(request, 'page2.html', {
+        'info': 'Ми продаємо найкращі рослини 🌿',
+        'categories': categories
+    })
